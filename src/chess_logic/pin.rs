80da1ef9ec6319701754314e472b7e439c487b59
aspiringLich <48413902+aspiringLich@users.noreply.github.com>
2022-06-11 12:48:29 -0400

    Fix Pins

--- src/chess_logic/pin.rs
+++ src/chess_logic/pin.rs
@@ -42,7 +42,7 @@ impl ChessState {
         let pieces: &[Vec<Piece>; 2] = unsafe { std::mem::transmute(&self.pieces) };
 
         // closest pieces
-        let mut closest: [[Option<(usize, i8)>; 9]; 2] = [[None; 9]; 2];
+        let mut closest: [[Option<(usize, u8)>; 9]; 2] = [[None; 9]; 2];
 
         self.pinned_pieces = vec![PinType::None; self.pieces[self.turn as usize].len()];
 
@@ -51,15 +51,14 @@ impl ChessState {
             for (i, piece) in pieces[team].iter().enumerate() {
                 let (x, y) = piece.rel_from(self.king(self.turn));
                 if is_45(x, y) {
-                    let max = max(x.abs(), y.abs());
+                    let max = max(x.abs() as u8, y.abs() as u8);
                     let index = coord_to_index(x, y);
 
-                    let item: &mut Option<(usize, i8)> = &mut closest[team][coord_to_index(x, y)];
-                    // if theres something there already and its further away, update it
+                    let item: &mut Option<(usize, u8)> = &mut closest[team][coord_to_index(x, y)];
+                    // if theres something there already set the distance to max to show its invalid
+                    // if its our own guy
                     if let Some(x) = item {
-                        if x.1 > max {
-                            *x = (i, max)
-                        }
+                        *x = if team == self.turn() { (i, u8::MAX) } else { (i, max) };
                     // otherwise if theres nothing there update it
                     } else {
                         *item = Some((i, max));
@@ -94,25 +93,24 @@ impl ChessState {
             .enumerate()
             .filter(|(i, x)| x.0.is_some() && x.1.is_some())
         {
-            let (i, (Some(closest_white), Some(closest_black))) = item else { unreachable!() };
+            let (i, (Some(closest_black), Some(closest_white))) = item else { unreachable!() };
 
             // the index / distance of the closest pieces
             let piece_index = [closest_white.0, closest_black.0];
             let distance = [closest_white.1, closest_black.1];
 
-            // if theres a possible threat
-            if possible_threat(self.pieces[self.opp_turn()][piece_index[self.turn()]].variant(), i)
+            // if theres a possible threat and the opposite team is closer
+            if possible_threat(self.pieces[self.opp_turn()][piece_index[self.opp_turn()]].variant(), i)
+                && distance[self.turn()] != u8::MAX // our closest person is not invalid
+                && distance[self.opp_turn()] > distance[self.turn()]
             {
                 dbg!(self.pieces[self.turn as usize][piece_index[self.turn as usize]]);
 
                 // get the piece we would like the modify
                 let turn = self.turn();
-                let mod_pin = &mut self.pinned_pieces[piece_index[turn]];
 
                 use PinType::*;
-                if *mod_pin == None {
-                    let mod_pin = Pinned(index_to_coord(i));
-                }
+                self.pinned_pieces[piece_index[turn]] = Pinned(index_to_coord(i));
             }
         }
     }
