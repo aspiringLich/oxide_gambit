9538d5061066cdaad2fdac2877ba208445cca3ba
aspiringLich <48413902+aspiringLich@users.noreply.github.com>
2022-07-21 11:02:59 -0400

    Update Constantds

--- src/ai/mod.rs
+++ src/ai/mod.rs
@@ -15,6 +15,6 @@ pub struct ComputeMove(Task<ChessMove>);
 const DEBUG: bool = true;
 
 const PIECE_VALUE: [f32; 7] = [0.0, 1.0, 3.2, 3.3, 5.0, 9.0, 0.0];
-const SQUARE_WEIGHT: f32 = 1.0 / 200.0;
-const THREATENED_WEIGHT: f32 = 1.0 / 2.5;
-const PROTECTED_WEIGHT: f32 = 1.0 / 4.0;
+const SQUARE_WEIGHT: f32 = 1.0 / 300.0;
+const THREATENED_WEIGHT: f32 = 1.0 / 6.5;
+const PROTECTED_WEIGHT: f32 = 1.0 / 8.0;
