6dd5669a7d2ee67513ac451ab5059e59ab060c88
aspiringLich <sirbrandonthenerd@gmail.com>
2023-03-11 19:14:37 -0500

    fix directioniter

--- src/chess/board.rs
+++ src/chess/board.rs
@@ -17,30 +17,6 @@ impl<T: BoardType> Board<T> {
     }
 }
 
-pub struct DirectionIterator<'a, T: BoardType> {
-    board: &'a Board<T>,
-    dx: i8,
-    dy: i8,
-    x: u8,
-    y: u8,
-}
-
-impl<'a, T: BoardType> Iterator for DirectionIterator<'a, T> {
-    type Item = T;
-    fn next(&mut self) -> Option<Self::Item> {
-        let x = (self.x as i8) + self.dx;
-        (x >= 0 && x < 8).then(|| {
-            self.x = x as u8;
-        });
-        let y = (self.y as i8) + self.dy;
-        (y >= 0 && y < 8).then(|| { 
-            self.y = y as u8;
-        });
-
-        Some(self.board[(y * 8 + x) as usize])
-    }
-}
-
 impl<T: BoardType> Board<T> {
     pub fn get<I: BoardIndex>(&self, square: I) -> Option<&T> {
         self.squares.get(square.get())
@@ -48,10 +24,6 @@ impl<T: BoardType> Board<T> {
 
     pub fn get_mut<I: BoardIndex>(&mut self, square: I) -> Option<&mut T> {
         self.squares.get_mut(square.get())
-    }
-
-    pub fn iter_direction<'a, I: BoardIndex>(&'a self, dir: Direction, start: I) -> DirectionIterator<'a, T> {
-        DirectionIterator { board: self, dx: dir.x(), dy: dir.y(), x: (start.get() % 8) as u8, y: (start.get() / 8) as u8 }
     }
 }
 
@@ -95,17 +67,5 @@ mod test {
         assert_eq!(board[Square(0)], 1);
         assert_eq!(board.get(Square(255)), None);
         assert_eq!(board.get(Square(0)), Some(&1));
-    }
-
-    #[test]
-    fn test_direction_iter() {
-        let mut board: Board<i32> = Board::new();
-        let mut i = 0;
-        board.squares = board.squares.map(|_| {
-            i += 1;
-            i - 1
-        });
-        
-        assert_eq!(board.iter_direction(Direction::NE, 0).collect::<Vec<_>>(), vec![0, 9, 18, 27, 36, 45, 54, 63]);
     }
 }
--- src/chess/direction.rs
+++ src/chess/direction.rs
@@ -1,5 +1,7 @@
 
 
+#[allow(dead_code)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Direction {
     E,
     NE,
@@ -12,34 +14,75 @@ pub enum Direction {
 }
 
 use Direction::*;
+
+use super::board::{Board, self};
 impl Direction {
     const ORTHOGONAL: [Direction; 4] = [E, N, W, S];
     const DIAGONAL: [Direction; 4] = [NE, NW, SW, SE];
     const ALL: [Direction; 8] = [E, NE, N, NW, W, SW, S, SE];
     
-    pub fn x(&self) -> i8 {
+    pub fn xy(self) -> (i8, i8) {
         match self {
-            E => 1,
-            NE => 1,
-            N => 0,
-            NW => -1,
-            W => -1,
-            SW => -1,
-            S => 0,
-            SE => 1
+            E => (1, 0),
+            NE => (1, 1),
+            N => (0, 1),
+            NW => (-1, 1),
+            W => (-1, 0),
+            SW => (-1, -1),
+            S => (0, -1),
+            SE => (1, -1)
         }
     }
     
-    pub fn y(&self) -> i8 {
-        match self {
-            E => 0,
-            NE => 1,
-            N => 1,
-            NW => 1,
-            W => 0,
-            SW => -1,
-            S => -1,
-            SE => -1
+    pub fn x(self) -> i8 {
+        self.xy().0
+    }
+    
+    pub fn y(self) -> i8 {
+        self.xy().1
+    }
+}
+
+pub struct DirectionIterator<'a, T: board::BoardType> {
+    board: &'a Board<T>,
+    dx: i8,
+    dy: i8,
+    x: i8,
+    y: i8,
+}
+
+impl<'a, T: board::BoardType> Iterator for DirectionIterator<'a, T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.x < 0 || self.x >= 8 || self.y < 0 || self.y >= 8 {
+            return None;
         }
+        let out = Some(self.board[(self.y * 8 + self.x) as usize]);
+        self.x += self.dx;
+        self.y += self.dy;
+        out
+    }
+}
+
+impl<T: board::BoardType> Board<T> {
+    pub fn iter_direction<'a, I: board::BoardIndex>(&'a self, dir: Direction, start: I) -> DirectionIterator<'a, T> {
+        DirectionIterator { board: self, dx: dir.x(), dy: dir.y(), x: (start.get() % 8) as i8, y: (start.get() / 8) as i8 }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::chess::{board::Board, direction::Direction};
+
+    #[test]
+    fn test_direction_iter() {
+        let mut board: Board<i32> = Board::new();
+        let mut i = 0;
+        board.squares = board.squares.map(|_| {
+            i += 1;
+            i - 1
+        });
+        
+        assert_eq!(board.iter_direction(Direction::NE, 0).collect::<Vec<_>>(), vec![0, 9, 18, 27, 36, 45, 54, 63]);
     }
 }
